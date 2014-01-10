570036a9e026e34fc35fdbf1d6acc77674b121ce
Elli <Elli@Ellis-MacBook-Air.local>
2014-01-10 11:57:06 -0800

    lists all previous todo submissions

--- todo.py
+++ todo.py
@@ -10,7 +10,12 @@ def index():
 @app.route("/create", methods=["POST"])
 def create():
 	to_do = request.form["to_do"]
-	return render_template("index.html", to_do=to_do)
+	f = open("db.txt", 'a')
+	f.write("%s\n" % to_do)
+	f.close()
+	f = open("db.txt")
+	items = f.readlines()
+	return render_template("index.html", to_do=items)
 
 
 if __name__ == "__main__":
